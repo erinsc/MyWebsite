fd5507651958edcd2812233880a490fbe2d65aa1
gronktonkbabonk <gronktonkbabonk@proton.me>
2026-03-23 21:25:56 +0000

    waterfalls and about me

--- fern/js/onload.js
+++ fern/js/onload.js
@@ -1,14 +1,28 @@
-let welcome = document.getElementById("welcomeMessage")
+{
+    let welcome = document.getElementById("welcomeMessage")
+    const welcomeMessages = [
+        "Welcome to my website!!",
+        "Mewcome to my website!!",
+        "Meowcome!",
+        "Welcome!!",
+        "Welcome to Fern's nook!",
+        "Meowcome to Fern's nook!",
+    ]
+    welcome.textContent = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]
+}
 
-const welcomeMessages = [
-    "Welcome to my website!!",
-    "Mewcome to my website!!",
-    "Meowcome!",
-    "Welcome!!",
-    "Welcome to Fern's nook!",
-    "Meowcome to Fern's nook!",
-]
+{
+    let myTimeSpan = document.getElementById("myTime")
+    let timeOffsetSpan = document.getElementById("hoursChange")
+    let stringPadding = ""
+    const ukDate = new Date().toLocaleString("en-GB", { timeZone: "Europe/London", hour: "numeric", minute: "numeric", second: "numeric", hour12: false });
+    const currentHour = new Date().getHours()
+    const ukHour = parseInt(ukDate.split(":")[0])
+    console.log(ukHour)
+    const hourOffset = (ukHour - currentHour) || 0
+    if (hourOffset < 0) { stringPadding = `${hourOffset} hours ahead of me` } else { stringPadding = `${-hourOffset} hours behind me` }
+    myTimeSpan.textContent = ukDate
+    timeOffsetSpan.textContent = stringPadding
+}
 
-welcome.textContent = welcomeMessages[Math.floor(Math.random()*welcomeMessages.length)]
-
-document.querySelectorAll('a').forEach(link => {link.target = "_blank";}); // im lazy
+document.querySelectorAll('a').forEach(link => { link.target = "_blank"; }); // im lazy
