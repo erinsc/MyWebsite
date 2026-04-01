f34a88987a292e9b7ea55faecb0d8e30184af1ba
gronktonkbabonk <gronktonkbabonk@proton.me>
2026-04-01 01:21:41 +0100

    changed sidebar to be on da side

--- fern/js/universalOnLoad.js
+++ fern/js/universalOnLoad.js
@@ -2,6 +2,6 @@
     fetch('/html/navbar.html')
     .then(r => r.text())
     .then(html => {
-        document.getElementById('NAVBAR_PLACEHOLDER').innerHTML = html;
+        document.getElementById('NAVBAR_PLACEHOLDER').outerHTML = html;
     });
 }
